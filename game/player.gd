844e607a27aff2e7169f75e6b8cd4f2b289d6428
Nico <88898201+EinfachNurN1C0@users.noreply.github.com>
2025-02-27 23:18:20 +0100

    .

--- game/player.gd
+++ game/player.gd
@@ -1,5 +1,4 @@
 extends Node2D
-
 @export var speed = 400
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,9 +9,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("w"):
-		velocity.y += 1
-	if Input.is_action_pressed("s"):
 		velocity.y -= 1
+	if Input.is_action_pressed("s"):
+		velocity.y += 1
 	if Input.is_action_pressed("a"):
 		velocity.x -= 1
 	if Input.is_action_pressed("d"):
